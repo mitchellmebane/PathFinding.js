80fceccbde2f7e5656463c0fa85c0bc19b34ebb0
Mitchell Mebane <mitchell.mebane@gmail.com>
2014-08-30 22:11:32 +0100

    Fix build for browserify 5

--- utils/build.js
+++ utils/build.js
@@ -1,36 +1,39 @@
 #!/usr/bin/env node
 
-var fs         = require('fs');
-var path       = require('path');
-var uglifyjs   = require('uglify-js');
+var fs = require('fs');
+var path = require('path');
+var uglifyjs = require('uglify-js');
 var browserify = require('browserify');
 
 function bundle(file, callback) {
-  var opts = { standalone: 'PF' };
-  browserify(file).bundle(opts, callback);
+    var opts = {
+        standalone: 'PF', 
+        entries: [file]
+    };
+    browserify(opts).bundle(callback);
 }
 
 function addBanner(source) {
-  var banner = fs.readFileSync(__dirname + '/banner').toString();
-  return banner + source;
+    var banner = fs.readFileSync(__dirname + '/banner').toString();
+    return banner + source;
 }
 
 function minify(source) {
-  var opts = { fromString: true };
-  return uglifyjs.minify(source, opts).code;
+    var opts = { fromString: true };
+    return uglifyjs.minify(source, opts).code;
 }
 
 function build(dest, options) {
-  options = options || {};
+    options = options || {};
 
-  var src = __dirname + '/../src/PathFinding.js';
+    var src = __dirname + '/../src/PathFinding.js';
 
-  bundle(src, function (err, bundled) {
-    var bannered = addBanner(bundled);
-    var content = options.minify ? minify(bannered) : bannered;
-    fs.writeFileSync(dest, content);
-    console.log('built', path.resolve(dest));
-  });
+    bundle(src, function(err, bundled) {
+        var bannered = addBanner(bundled);
+        var content = options.minify ? minify(bannered) : bannered;
+        fs.writeFileSync(dest, content);
+        console.log('built', path.resolve(dest));
+    });
 }
 
 build(__dirname + '/../lib/pathfinding-browser.js');
